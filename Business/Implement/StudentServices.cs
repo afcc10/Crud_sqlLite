bc78e6f33dd56e07785e2c56fd7dfc863d16df96
Andres Felipe Cabezas Casas <afcabezas@indracompany.com>
2022-10-10 19:16:37 -0500

    Agregar archivos de proyecto.

---
+++ Business/Implement/StudentServices.cs
@@ -0,0 +1,58 @@
+﻿using Business.Contract;
+using Common.Utilities.Services;
+using DataAccess.Core.Contract;
+using Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Implement
+{
+    public class StudentServices : IStudentServices
+    {
+        #region Propierties
+        private readonly IStudentRepository _studentRepository;
+        #endregion
+
+        #region Constructor
+        public StudentServices(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+
+        
+        #endregion
+
+        public async Task<Response<List<StudentDto>>> GetStudents()
+        {
+            var result = await _studentRepository.GetStudents();
+            return result;
+        }
+
+        public async Task<Response<bool>> UpdateStudent(StudentDto student)
+        {
+            var result = await _studentRepository.UpdateStudent(student);
+            return result;
+        }
+
+        public async Task<Response<bool>> CreateStudent(StudentDto student)
+        {
+            var result = await _studentRepository.CreateStudent(student);
+            return result;
+        }
+
+        public async Task<Response<bool>> DeleteByIdStudent(int id)
+        {
+            var result = await _studentRepository.DeleteByIdStudent(id);
+            return result;
+        }
+
+        public async Task<Response<StudentDto>> GetByIdStudent(int id)
+        {
+            var result = await _studentRepository.GetByIdStudent(id);
+            return result;
+        }
+    }
+}
---
+++ DependencyInjection/IoCRegister.cs
@@ -0,0 +1,39 @@
+﻿using Business.Contract;
+using Business.Implement;
+using DataAccess.Core.Contract;
+using DataAccess.Core.Implements;
+using DataAccess.Models;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DependencyInjection
+{
+    public static class IoCRegister
+    {
+        public static IServiceCollection AddRepository(IServiceCollection services)
+        {
+            services.AddScoped<IStudentRepository, StudentRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddServices(IServiceCollection services)
+        {
+            services.AddScoped<IStudentServices, StudentServices>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddDbContext(IServiceCollection services, string DefaultConnection)
+        {
+            var connection = new SqliteConnection(DefaultConnection);
+            connection.Open();
+            connection.EnableExtensions(true);
+            services.AddDbContext<DbCrudContext>(options => options.UseSqlite(DefaultConnection));           
+
+            return services;
+        }
+    }
+}
